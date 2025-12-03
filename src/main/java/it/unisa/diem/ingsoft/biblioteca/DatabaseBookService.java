388658ef766b3b8c85e915731e45251a022b6046
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-03 17:39:51 +0100

    Corretto il metodo removeByISBN (rimuoveva il libro solo dalla copia creata, non nel database), aggiunti i commenti ai metodi implementati in DatabaseBookService.

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/DatabaseBookService.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/DatabaseBookService.java
@@ -4,14 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @brief Implementazione del servizio di gestione dei libri che interagisce con il
+ * database.
+ * * La classe fornisce metodi per ottenere libri presenti nel database
+ * secondo un criterio di ricerca specificato.
+ * Viene inoltre fornito un metodo per la rimozione dei libri memorizzati nel database.
+ */
 public class DatabaseBookService implements BookService {
 
+    /**
+     * @brief Riferimento al database utilizzato per l'accesso ai dati.
+     */
     private final Database database;
 
+    /**
+     * @brief Costrutore della classe DatabaseBookService.
+     * * @param database L'istanza del database da utilizzare per le operazioni sui libri.
+     */
     public DatabaseBookService(Database database) {
         this.database = database;
     }
 
+    /**
+     * @brief Recupera tutti i libri presenti nel database.
+     * * Esegue una query SQL per ottenere l'elenco completo di tutti i libri.
+     * * @return Una lista di libri contenente tutti i libri del databse.
+     */
     public List<Book> getAll() {
         return this.database.getJdbi()
                 .withHandle(handle -> handle.createQuery("SELECT * FROM books")
@@ -19,6 +38,13 @@ public class DatabaseBookService implements BookService {
                         .list());
     }
 
+    /**
+     * @brief Recupera un libro tramite il suo codice ISBN.
+     * * Il metodo itera su tutti i libri ottenuti e ne cerca uno
+     * corispondente all'ISBN fornito.
+     * * @param ISBN Il codice ISBN (Stringa) del libro da cercare.
+     * @return Un Optional<Book> che contiene il libro se trovato, altrimenti Optional.empty().
+     */
     public Optional<Book> getByISBN(String ISBN){
         return this.getAll()
                 .stream()
@@ -26,6 +52,12 @@ public class DatabaseBookService implements BookService {
                 .findFirst();
     }
 
+    /**
+     * @brief Recupera tutti i libri scritti da un determinato autore.
+     * * Itera su tutti i libri presenti nel database e filtra per l'autore specificato.
+     * * @param author Il nome dell'autore (Stringa) da cercare.
+     * @return Una lista di libri contenente i libri dell'autore specificato.
+     */
     public List<Book> getByAuthor(String author){
         List<Book> listByAuthors = new ArrayList<>();
         for(Book book : this.getAll()){
@@ -37,6 +69,12 @@ public class DatabaseBookService implements BookService {
         return listByAuthors;
     }
 
+    /**
+     * @brief Recupera tutti i libri appartenenti a un determinato genere.
+     * * Itera su tutti i libri e filtra in base al genere specificato.
+     * * @param genre Il genere (Stringa) dei libri da cercare.
+     * @return Una lista di libri contenente i libri del genere specificato.
+     */
     public List<Book> getByGenre(String genre){
         List<Book> listByGenre = new ArrayList<>();
         for(Book book : this.getAll()){
@@ -48,6 +86,12 @@ public class DatabaseBookService implements BookService {
         return listByGenre;
     }
 
+    /**
+     * @brief Recupera tutti i libri pubblicati in un anno specifico.
+     * * Itera su tutti i libri e filtra in base all'anno di pubblicazione specificato.
+     * * @param releaseYear L'anno di pubblicazione (intero) da cercare.
+     * @return Una lista di libri contenente i libri pubblicati nell'anno specificato.
+     */
     public List<Book> getByReleaseYear(int releaseYear){
         List<Book> listByReleaseYear = new ArrayList<>();
         for(Book book : this.getAll()){
@@ -59,6 +103,12 @@ public class DatabaseBookService implements BookService {
         return listByReleaseYear;
     }
 
+    /**
+     * @brief Recupera tutti i libri con un determinato titolo.
+     * * Itera su tuti i libri e filtra in base al titolo specificato.
+     * * @param title Il titolo (Stringa) dei libri da cercare.
+     * @return Una lista di libri contenente i libri con il titolo specificato.
+     */
     public List<Book> getByTitle(String title){
         List<Book> listByTitle = new ArrayList<>();
         for(Book book : this.getAll()){
@@ -70,8 +120,16 @@ public class DatabaseBookService implements BookService {
         return listByTitle;
     }
 
-    public void removeByISBN(String ISBN){
-        this.getAll().removeIf(book -> book.getISBN().equals(ISBN));
+    /**
+     * @brief Rimuove un libro dal database basandosi sul suo codice ISBN.
+     * * Esegue un'istruzione SQL DELETE sul database per rimuovere
+     * in modo permanente il record corrispondente all'ISBN.
+     * * @param ISBN Il codice ISBN (Stringa) del libro da rimuovere.
+     */
+    public boolean removeByISBN(String ISBN){
+        return this.database.getJdbi()
+                .withHandle(handle -> handle.createUpdate("DELETE FROM books WHERE ISBN = :isbn")
+                        .bind("isbn", ISBN)
+                        .execute()) > 0;
     }
 }
-
