91bc002ff53f8a3cfb53e2f50f451053b82e2ec8
Nick0471 <nicolomassimolisena@gmail.com>
2025-12-04 10:13:26 +0100

    Aggiunto BookSceneController

---
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/BookSceneController.java
@@ -0,0 +1,25 @@
+package it.unisa.diem.ingsoft.biblioteca;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class BookSceneController extends GUIController{
+    @FXML private ComboBox<String> searchType;
+    @FXML private TextField searchField;
+
+    @FXML private TableView<Book> BookCatalog;
+
+    @FXML private TableColumn<Book, String> ColumnTitle;
+    @FXML private TableColumn<Book, String> ColumnAuthor;
+    @FXML private TableColumn<Book, String> ColumnGenre;
+    @FXML private TableColumn<Book, Integer> ColumnYear;
+    @FXML private TableColumn<Book, String> ColumnISBN;
+    @FXML private TableColumn<Book, Integer> ColumnTotalCopies;
+    @FXML private TableColumn<Book, Integer> ColumnCopies;
+    @FXML private TableColumn<Book, String> ColumnDescription;
+
+    @FXML private Button btnHome;
+    @FXML private Button btnAdd;
+    @FXML private Button btnModify;
+    @FXML private Button btnRemove;
+}
