cea78b9b0e16b70931d19dbaa15efc73e1fd4623
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-04 11:06:31 +0100

    Aggiunta la classe BookMapper, implementato il metodo map, aggiunto il commento del suddetto metodo.

---
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/BookMapper.java
@@ -0,0 +1,37 @@
+package it.unisa.diem.ingsoft.biblioteca;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+public class BookMapper implements RowMapper<Book> {
+    /**
+     * @brief Esegue la mappatura di una singola riga del ResultSet su un oggetto Book.
+     */
+    @Override
+    public Book map(ResultSet rs, StatementContext ctx) throws SQLException {
+        Book book = new Book();
+
+        String isbn = rs.getString("isbn");
+        String title = rs.getString("title");
+        String author = rs.getString("author");
+        String genre = rs.getString("genre");
+        String description = rs.getString("description");
+        int releaseYear = rs.getInt("releaseYear");
+        int totalCopies = rs.getInt("totalCopies");
+        int remainingCopies = rs.getInt("remainingCopies");
+
+        book.setIsbn(isbn);
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setGenre(genre);
+        book.setDescription(description);
+        book.setReleaseYear(releaseYear);
+        book.setTotalCopies(totalCopies);
+        book.setRemainingCopies(remainingCopies);
+
+        return book;
+    }
+}
