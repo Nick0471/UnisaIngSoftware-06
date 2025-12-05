5a2f819422c083df00db58d00218a2a1895de3ff
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-05 16:05:54 +0100

    Aggiunti i commenti alla classe UnsetPasswordException

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/UnsetPasswordException.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/UnsetPasswordException.java
@@ -1,6 +1,16 @@
 package it.unisa.diem.ingsoft.biblioteca.exception;
 
+/**
+ * @brief Eccezione lanciata in fase di esecuzione quando il sistema tenta di verificare
+ * la password del bibliotecario, ma scopre che la password non è stata mai impostata
+ * o è assente nel database.
+ */
 public class UnsetPasswordException extends RuntimeException {
+
+    /**
+     * @brief Costruttore di default, inizializza l'eccezione con un messaggio di errore
+     * standard che notifica l'assenza della password nel database.
+     */
     public UnsetPasswordException() {
         super("Non è presente alcuna password nel database");
     }
