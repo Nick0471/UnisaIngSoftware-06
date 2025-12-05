7435403061eb0d57920bae2d4e5deea13f47368e
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-05 16:24:36 +0100

    Aggiunti commenti alla classe UserMapper

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/mapper/UserMapper.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/mapper/UserMapper.java
@@ -7,15 +7,20 @@ package it.unisa.diem.ingsoft.biblioteca.mapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import it.unisa.diem.ingsoft.biblioteca.model.Loan;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
 import it.unisa.diem.ingsoft.biblioteca.model.User;
 
 /**
- * @brief Esegue la mappatura di una singola riga del ResultSet su un oggetto User
+ * @brief Mapper che realizza la corrispondenza tra i dati salvati nelle colonne del
+ * database della tabella 'users' e un oggetto del modello {@link User}.
  */
 public class UserMapper implements RowMapper<User> {
+    /**
+     * @brief Esegue la mappatura di una singola riga del ResultSet su un oggetto User
+     */
 	@Override
 	public User map(ResultSet rs, StatementContext ctx) throws SQLException {
         User user = new User();
