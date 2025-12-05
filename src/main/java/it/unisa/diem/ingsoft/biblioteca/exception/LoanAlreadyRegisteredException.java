311b26cde2cc7def03f5be436590df8d752e8829
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-05 15:53:25 +0100

    Aggiunti i commenti alla classe LoanAlreadyRegisteredException

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/LoanAlreadyRegisteredException.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/LoanAlreadyRegisteredException.java
@@ -1,6 +1,16 @@
 package it.unisa.diem.ingsoft.biblioteca.exception;
 
+/**
+ * @brief Eccezione lanciata quando si tenta di registrare un nuovo prestito, ma ne esiste già uno attivo
+ * per la combinazione specificata di utente e libro.
+ */
 public class LoanAlreadyRegisteredException extends LoanException {
+
+    /**
+     * @brief Costruttore di default, inizializza l'eccezione con un messaggio di errore
+     * standard che notifica la presenza di un prestito già registrato per l'utente e
+     * il libro specificati.
+     */
 	public LoanAlreadyRegisteredException() {
 		super("Esiste già un prestito per l'utente ed il libro specificati");
 	}
