73e667e7abd63945a74041f5db3b7504e3067076
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-05 15:57:57 +0100

    Aggiunti i commenti alla classe UnknownLoanException

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/UnknownLoanException.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/UnknownLoanException.java
@@ -1,6 +1,17 @@
 package it.unisa.diem.ingsoft.biblioteca.exception;
 
+/**
+ * @brief Eccezione lanciata quando un'operazione (es. restituzione o aggiornamento del prestito)
+ * viene richiesta per un prestito che non esiste o non è attivo per la combinazione specificata
+ * di utente e libro. Indica, quindi, che il record del prestito atteso non è stato
+ * trovato nel database.
+ */
 public class UnknownLoanException extends LoanException {
+
+    /**
+     * @brief Costruttore di default, inizializza l'eccezione con un messaggio di errore
+     * standard che notifica l'assenza di un prestito per l'utente e il libro specificati.
+     */
 	public UnknownLoanException() {
 		super("Non esiste alcun prestito tra utente e libro specificati");
 	}
