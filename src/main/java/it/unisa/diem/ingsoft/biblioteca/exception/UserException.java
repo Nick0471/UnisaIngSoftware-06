962f0d44ffbbfc1b8d5103941b883ef7f0799b0f
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-05 16:08:05 +0100

    Aggiunti i commenti alla classe UserException

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/UserException.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/UserException.java
@@ -1,6 +1,17 @@
 package it.unisa.diem.ingsoft.biblioteca.exception;
 
+/**
+ * @brief Eccezione base per tutti gli errori specifici relativi alla gestione degli
+ * utenti.
+ */
 public class UserException extends Exception {
+
+    /**
+     * @brief Costruttore con messaggio.
+     *Inizializza l'eccezione UserException con un messaggio specifico.
+     *
+     * @param message Il messaggio che descrive l'errore specifico dell'utente.
+     */
     public UserException(String message) {
         super(message);
     }
