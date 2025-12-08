e6d9a671e0b0575148c74bda46eb38fedd5750dc
Nicola <nicola.picarella6@gmail.com>
2025-12-08 12:15:59 +0100

    rimossi spazi inutili in DatabaseBookService.java

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/service/DatabaseBookService.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/service/DatabaseBookService.java
@@ -143,9 +143,6 @@ public class DatabaseBookService implements BookService {
                                         + "WHERE isbn = :isbn")
                             .bind("isbn", isbn)
                             .execute()) > 0;
-
-
-
     }
 
     /**
