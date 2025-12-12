c8db76db1a847fbebec3cd6809aead016d985984
Cres <vincenzo.daniel.raimo@gmail.com>
2025-12-12 09:49:58 +0100

    Aggiunti i commenti alla classe InvalidIdException

--- src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/InvalidIdException.java
+++ src/main/java/it/unisa/diem/ingsoft/biblioteca/exception/InvalidIdException.java
@@ -1,6 +1,15 @@
 package it.unisa.diem.ingsoft.biblioteca.exception;
 
+/**
+ * @brief Ecceziona lanciata quando si tenta una modifica relativa ad un utente usando
+ *  una matricola non valida.
+ */
 public class InvalidIdException extends Exception {
+
+    /**
+     * @brief Costruttore di default, inizializza l'eccezione con un messaggio di errore
+     * standard che notifica che la matricola non è valida.
+     */
     public InvalidIdException() {
         super("La matricola inserita per la registrazione non è valida!");
     }
